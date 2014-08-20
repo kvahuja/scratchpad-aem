16b6d80de94ae9146556b9e78548635af2254610
Kapil Viren Ahuja <kahuja1@sapient.com>
2014-08-20 10:03:36 +0530

    added the default project structure for arm development using maven archetype (still needs cleanup of reactor files)

---
+++ aemspad-core/bundle/src/main/java/com/scratchpad101/aem6spad/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.scratchpad101.aem6spad;
+
+import aQute.bnd.annotation.Version;
---
+++ aemspad-core/bundle/src/test/java/com/scratchpad101/aem6spad/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.scratchpad101.aem6spad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
